7e8c07e6414b062e7a11a117791b31f6239202d6
camila-braz <camila@numera.consulting>
2021-12-07 21:53:30 -0300

    commit 2 teste

--- howdy/urls.py
+++ howdy/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import image_load
+#from .views import image_load
+from .views import homePageView
 
 urlpatterns = [
-   # path("", homePageView, name="home"),
-    path("" , image_load, name='image_load'),
+    path("", homePageView, name="home"),
+    #path("" , image_load, name='image_load'),
 ]
 
